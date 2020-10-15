58570b8c43af8113dd124bd0e2a2ce783e3d4cf2
LoGinto <46216287+LoGinto@users.noreply.github.com>
2020-10-15 14:45:59 +0400

    Shooting Setup/RaycastSetup

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Float vars")]
+    public float damage = 15f;
+    public float shotDistance = 20;
+    public float rpm = 60;
+    private float secondsBetweenShots;
+    private float nextPossibleShootTime;
+    Ray ray;
+    [Header("Other vars")]
+    public Camera cam;
+
+    public bool isPlayer= true;
+    RaycastHit hit;
+    public enum ShootType
+    {
+        Semi, Burst, Auto
+    }
+    public ShootType shootType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsBetweenShots = 60 / rpm;     
+        if(cam == null)
+        {
+            cam = GameObject.FindGameObjectWithTag("AimCam").GetComponent<Camera>();
+        }
+    }
+    private bool CanShoot()
+    {
+        bool canShoot = true;
+        if (Time.time < nextPossibleShootTime)
+        {
+            canShoot = false;
+        }
+        else
+        {
+            canShoot = true;
+        }
+        return canShoot;
+    }
+    public void Shoot()
+    {
+        Debug.Log("Can shoot " + CanShoot());
+        if (CanShoot())
+        {
+            if (isPlayer)
+            {
+                //player shooting here 
+                ray = new Ray(cam.transform.position, cam.transform.forward);
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    Debug.DrawRay(cam.transform.position, cam.transform.forward);
+                    Debug.Log(hit.transform.name);
+                }
+                nextPossibleShootTime = Time.time + secondsBetweenShots;
+            }
+            else
+            {
+                //enemy shooting here
+                //shooting by reference in ai script ofc
+            }
+            
+        }      
+    }
+    public void ShootContinious()
+    {
+        if (shootType == ShootType.Auto)
+        {
+            Shoot();
+        }
+    }
+}
--- Assets/Scripts/WeaponAnimationHandler.cs
+++ Assets/Scripts/WeaponAnimationHandler.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WeaponAnimationHandler : MonoBehaviour
 {
     Animator animator;
+    Gun gun = null;
+    Transform weaponHolder;
     bool isAiming = false;
     [SerializeField] float xRotationMultiplier = 3f;
     [Header("To disable")]
@@ -12,6 +14,7 @@ public class WeaponAnimationHandler : MonoBehaviour
     [SerializeField] GameObject cineCamToDisable;
     [Header("To enable")]
     [SerializeField] GameObject camtoEnable;
+    [SerializeField] CanvasGroup reticleImageCanvasGroup;
     Camera myCamera;
     [SerializeField] GameObject cineCamToEnable;
     private void Start()
@@ -19,10 +22,18 @@ public class WeaponAnimationHandler : MonoBehaviour
         myCamera = GameObject.FindObjectOfType<Camera>();
         animator = GetComponent<Animator>();
         SwitchCams();
+        weaponHolder = GameObject.FindGameObjectWithTag("WeaponHolder").transform;
     }
     private void Update()
     {
-        SwitchCams();       
+        SwitchCams();
+        foreach (Transform weapon in weaponHolder)
+        {
+            if (weapon.gameObject.activeSelf == true)
+            {
+                gun = weapon.gameObject.GetComponent<Gun>();
+            }
+        }
         if (!myCamera.isActiveAndEnabled||myCamera == null)
         {
             myCamera = GameObject.FindObjectOfType<Camera>();
@@ -39,6 +50,13 @@ public class WeaponAnimationHandler : MonoBehaviour
             {              
                 Vector3 cameraForward = Vector3.Scale(myCamera.transform.forward, new Vector3(1, 0, 1)).normalized;                
                 transform.LookAt(cameraForward + transform.position);
+                reticleImageCanvasGroup.alpha = 1;
+                reticleImageCanvasGroup.interactable = false;
+            }
+            else
+            {
+                reticleImageCanvasGroup.alpha = 0;
+                reticleImageCanvasGroup.interactable = true;
             }
             animator.SetBool("isAiming", isAiming);
         }
@@ -61,6 +79,21 @@ public class WeaponAnimationHandler : MonoBehaviour
         {
             float mousePos = Input.mousePosition.y / Screen.height;
             animator.SetFloat("Aim", mousePos);
+            try
+            {
+                if(Input.GetMouseButtonDown(0)&&gun != null)
+                {
+                    gun.Shoot();
+                }
+                else if (Input.GetMouseButton(0) && gun !=null)
+                {
+                    gun.ShootContinious();
+                }
+            }
+            catch
+            {
+                Debug.Log("Catch executed in weapon animation handler");
+            }
         }
     }
     public bool GetIsAiming()
