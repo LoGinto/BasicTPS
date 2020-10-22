ccc78248aea073eaf8ca086c78911b1423a02e33
LoGinto <46216287+LoGinto@users.noreply.github.com>
2020-10-22 13:34:48 +0400

    Jump and ledge grab(just begun)

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]Camera myCamera;
+    [SerializeField] float jumpForwardDistance = 10;
+    [SerializeField] float jumpUpDist = 10;
+    private float distanceToGround;
+    Collider playerCollider;
+    private bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        playerCollider = gameObject.GetComponent<Collider>();
+        distanceToGround = playerCollider.bounds.extents.y;
+    }
+    public bool isOnGround()
+    {
+        //grounded or not
+        return Physics.Raycast(transform.position, -Vector3.up, distanceToGround + 0.1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        JumpForward();
+    }
+
+    private void JumpForward()
+    {
+        if (isOnGround() && animator.GetBool("isAiming") == false)
+        {
+            float verticalAxis = Input.GetAxis("Vertical");
+            float horizontalAxis = Input.GetAxis("Horizontal");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                Vector3 cameraForward = Vector3.Scale(myCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+                Vector3 udpatedVector = verticalAxis * cameraForward + horizontalAxis * myCamera.transform.right;
+                transform.LookAt(udpatedVector + transform.position);
+                animator.SetTrigger("JumpForward");
+                animator.applyRootMotion = true;
+                isJumping = true;
+                transform.position += transform.forward * Time.deltaTime * jumpForwardDistance;
+                transform.position += transform.up * Time.deltaTime * jumpUpDist;
+            }
+        }
+        else if (isOnGround() == false)
+        {
+            animator.applyRootMotion = false;
+            isJumping = false;
+            animator.ResetTrigger("JumpForward");
+        }
+    }
+    public bool IsJumping()
+    {
+        return isJumping;
+    }
+}
+
+    
---
+++ Assets/Scripts/Ledge.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    public LayerMask grabbableLayer;
+    [SerializeField] float rayCastDist = 4f;
+    [SerializeField] float heightOfJump = 3.5f;
+    [SerializeField] float minWidth = 2f;
+    RaycastHit hit;
+    Ray ray;
+    Animator animator;
+    Jump jumpScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        jumpScript = gameObject.GetComponent<Jump>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jumpScript.IsJumping())
+        {
+            ray = new Ray(transform.position,transform.forward);
+            if (Physics.Raycast(ray,out hit,rayCastDist,grabbableLayer))
+            {
+                //get height and width
+                GameObject hitObject = hit.collider.gameObject;
+                if (hitObject.GetComponent<BoxCollider>())
+                {
+                    BoxCollider box = hitObject.GetComponent<BoxCollider>();
+                    float height = hit.transform.localScale.y * ((BoxCollider)hit.collider).size.y;
+                    float width = hit.transform.localScale.z * ((BoxCollider)hit.collider).size.z;
+                    if (heightOfJump > height && width>minWidth)
+                    {
+                        Debug.Log("Grab");
+                    }
+                }
+            }
+        }        
+    }
+}
