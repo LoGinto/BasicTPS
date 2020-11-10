f3cd2478cbf8201f5e37e372145049ed0212fb88
LoGinto <46216287+LoGinto@users.noreply.github.com>
2020-11-10 17:10:29 +0400

    Helicopter 90% complete

---
+++ Assets/Scripts/TestScripts/EnterHeli.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterHeli : MonoBehaviour
+{
+    [SerializeField] float rayCastDist = 3f;
+    [SerializeField] GameObject[] cams;
+    [SerializeField] GameObject[] cinemachineCam;
+    // Start is called before the first frame update    
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        if(Physics.Raycast(ray,out hit, rayCastDist))
+        {
+            if (hit.collider.tag == "Helicopter")
+            {
+                if (Input.GetKeyDown(KeyCode.F) && hit.collider.GetComponent<Helicopter_Base>()) {
+                    hit.collider.GetComponent<Helicopter_Base>().SetEntryState(true);
+                    transform.parent = hit.collider.transform;
+                    this.gameObject.SetActive(false);
+                    foreach (GameObject cam in cams)
+                    {
+                        foreach(GameObject cinecam in cinemachineCam)
+                        {
+                            cam.SetActive(false);
+                            cinecam.SetActive(false);
+                        }
+                    }
+                   }
+            }
+        }
+    }
+   
+}
